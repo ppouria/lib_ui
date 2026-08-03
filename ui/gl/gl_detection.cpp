56c802af4c88eb92ebd874d042d38bdb90960a0c
Pouria Ebrahimzade <pouriaebrahimzade@yahoo.com>
2026-08-03 21:03:56 +0330

    Disable Qt RHI outside supported platforms

--- ui/gl/gl_detection.cpp
+++ ui/gl/gl_detection.cpp
@@ -333,7 +333,7 @@ Backend ChooseBackendDefault(Capabilities capabilities) {
 
 bool WidgetsRhiEnabled() {
 #if QT_VERSION >= QT_VERSION_CHECK(6, 7, 0)
-	if (!OptionUseQtRhi.value()) {
+	if (!OptionUseQtRhi.relevant() || !OptionUseQtRhi.value()) {
 		return false;
 	} else if (!Platform::IsMac()) {
 		if (ForceDisabled
